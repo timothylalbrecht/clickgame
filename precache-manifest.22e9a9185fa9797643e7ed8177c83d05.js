87cc2bdbf58f883d80e46555de15e9a378d675a7
timmy burrito <timothylalbrecht@gmail.com>
2019-01-29 13:34:27 -0500

    Updates

---
+++ precache-manifest.22e9a9185fa9797643e7ed8177c83d05.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e04d13d8cfe637cfec53251388bab8e",
+    "url": "/clickgame/static/media/logo.4e04d13d.png"
+  },
+  {
+    "revision": "0078e683936be3181461ef1a80d2a51c",
+    "url": "/clickgame/static/media/background4.0078e683.jpg"
+  },
+  {
+    "revision": "1d1da0236ccd610ca49c",
+    "url": "/clickgame/static/js/runtime~main.1d1da023.js"
+  },
+  {
+    "revision": "1605bc49a3a8a8eebdbe",
+    "url": "/clickgame/static/js/main.1605bc49.chunk.js"
+  },
+  {
+    "revision": "425193f9241e750fb12a",
+    "url": "/clickgame/static/js/1.425193f9.chunk.js"
+  },
+  {
+    "revision": "1605bc49a3a8a8eebdbe",
+    "url": "/clickgame/static/css/main.f4fe0e70.chunk.css"
+  },
+  {
+    "revision": "3237803bb9370018effc69ee90c69d5f",
+    "url": "/clickgame/index.html"
+  }
+];
